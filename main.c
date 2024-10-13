49882dcdaf4a8312fbbee5b951038b37bdaea4fa
Gabriel Nodari Pereira <144969599+GabeNodari@users.noreply.github.com>
2024-10-12 21:59:09 -0300

    main.c

--- main.c
+++ main.c
@@ -32,47 +32,52 @@ int get_life(int vida, int dano) {
 int main() {
   srand(time(NULL));
 
-  // Players com status randomizados, sendo que eles: não podem ser 0; precisam somar 20
+// Players com status randomizados, sendo que eles: não podem ser 0; precisam somar 20
 
-  // Player 1
-  int hp_P1 = get_random (1, 18);
-  int att_P1 = 19 - hp_P1;
-  int def_P1 = 20 - hp_P1 - att_P1;
+  int hp_P1, att_P1, def_P1;
+  int hp_P2, att_P2, def_P2;
 
-  // Player 2
-  int hp_P2 = get_random (1, 18);
-  int att_P2 = 19 - hp_P2;
-  int def_P2 = 20 - hp_P2 - att_P2;
+  do {
+    hp_P1 = get_random(1,18);
+    att_P1 = get_random(1,18);
+    def_P1 = get_random(1,18);
+  } while (hp_P1 + att_P1 + def_P1 != 18);
 
-  // Print stats
+  do {
+    hp_P2 = get_random(1,18);
+    att_P2 = get_random(1,18);
+    def_P2 = get_random(1,18);
+  } while (hp_P2 + att_P2 + def_P2 != 18);
+
+// Print stats
   printf("Os status do Player 1 sao: HP - %d, ATCK - %d, DEF - %d.\n", hp_P1, att_P1, def_P1);
   printf("Os status do Player 2 sao: HP - %d, ATCK - %d, DEF - %d.\n\n\n", hp_P2, att_P2, def_P2);
 
-  // Combate
+// Combate
   int toPlay = get_random(1,2);
 
-while (hp_P1 > 0 && hp_P2 > 0) {
-  if (toPlay == 1) {
-    int resultDamageP2 = get_damage (att_P1, def_P2);
-    hp_P2 = get_life (hp_P2, resultDamageP2);
-    printf("O Player 1 vai atacar!\n");
-    printf("Player 2 sofreu %d de dano.\n", resultDamageP2);
-    printf("A vida do Player 2 agora e %d.\n\n\n", hp_P2);
-    toPlay = 2;
+  while (hp_P1 > 0 && hp_P2 > 0) {
+    if (toPlay == 1) {
+      int resultDamageP2 = get_damage (att_P1, def_P2);
+      hp_P2 = get_life (hp_P2, resultDamageP2);
+      printf("O Player 1 vai atacar!\n");
+      printf("Player 2 sofreu %d de dano.\n", resultDamageP2);
+      printf("A vida do Player 2 agora e %d.\n\n\n", hp_P2);
+      toPlay = 2;
+    } else {
+        int resultDamageP1 = get_damage (att_P2, def_P1);
+        hp_P1 = get_life (hp_P1, resultDamageP1);
+        printf("O Jogador 2 vai atacar!\n");
+        printf("Player 1 sofreu %d de dano.\n", resultDamageP1);
+        printf("A vida do Player 1 agora e %d.\n\n\n", hp_P1);
+        toPlay = 1;
+    }
+  } if (hp_P1 == 0) {
+    printf("O Player 1 morreu.\n");
+    printf("O Player 2 venceu!\n");
   } else {
-      int resultDamageP1 = get_damage (att_P2, def_P1);
-      hp_P1 = get_life (hp_P1, resultDamageP1);
-      printf("O Jogador 2 vai atacar!\n");
-      printf("Player 1 sofreu %d de dano.\n", resultDamageP1);
-      printf("A vida do Player 1 agora e %d.\n\n\n", hp_P1);
-      toPlay = 1;
+      printf("O Player 2 morreu.\n");
+      printf("O Player 1 venceu!\n");
   }
-} if (hp_P1 == 0) {
-  printf("O Player 1 morreu.\n");
-  printf("O Player 2 venceu!\n");
-} else {
-  printf("O Player 2 morreu.\n");
-  printf("O Player 1 venceu!\n");
-}
   return 0;
 }
